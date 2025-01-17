fa1c4f1613c9369270ddd69d5f1d37c21c2ee441
Dmitry Zhivitsa <dev.zhivitsa@gmail.com>
2025-01-17 11:31:17 +0300

    task 2 oop modification get task sorting

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -33,7 +33,7 @@ class TaskManager:
 
     def get_tasks(self) -> List[Task]:
         """Возвращает список всех задач."""
-        return sorted(list(self.tasks.values()), key=lambda x:x[0])
+        return sorted(list(self.tasks.values()), key=lambda task: task.id)
 
     def add_task(self, task: Task):
         """Добавляет новую задачу, если её ID уникален."""
